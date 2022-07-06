0dec1c5afdff83cb426ec0b6d1e911628755fb37
KSRS CHARAN <51869689+ksrscharan@users.noreply.github.com>
2022-07-06 13:23:43 +0530

    Feature: Delete Button Addition (#1)
    * delete button addition
    
    * code format

--- src/context/Actions.tsx
+++ src/context/Actions.tsx
@@ -3,5 +3,6 @@ import { ActionType } from "./types";
 export const ACTION: ActionType = {
   ADD_DATA: "add-data",
   ADD_TO_CART: "add-to-cart",
+  DECREASE_COUNT: "decrease-count",
   DELETE_CART_ITEM: "delete-cart-item",
 };
